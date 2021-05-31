f829fbd9eb26a7019e4fa90f72e62b66b4068500
Jonathan Rovner <jonrovner@gmail.com>
2021-05-31 12:55:12 -0300

    prettier

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ app.get('/', (req, res) => {
 
 app.get('/api/random', async (req, res) => {
 
-try {
-    const queryString = `https://api.spoonacular.com/recipes/random?number=${req.query.number}&tags=${req.query.tags}&apiKey=${process.env.APIKEY}`
-    console.log(queryString)
-    const data = await fetch(queryString)
-    const results = await data.json() 
-    res.json(results)
+    try {
+        const queryString = `https://api.spoonacular.com/recipes/random?number=${req.query.number}&tags=${req.query.tags}&apiKey=${process.env.APIKEY}`
+        console.log(queryString)
+        const data = await fetch(queryString)
+        const results = await data.json() 
+        res.json(results)
     } catch (err) {
-    return res.status(500).json({
-        success: false,
-        message: err.message,
-    })
-    }
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+        }
 })
 
 app.get('/api/search', async (req, res) => {
